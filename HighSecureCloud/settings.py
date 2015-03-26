ed00e641bf9ff26f57d387abd8a43a7e8b8f74c5
Akhil Nadh PC <pcakhilnadh@gmail.com>
2015-03-26 23:24:20 +0530

    Update settings.py

--- HighSecureCloud/settings.py
+++ HighSecureCloud/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # for gmail 
 EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_HOST_USER="highsecurecloudstorage@gmail.com"
-EMAIL_HOST_PASSWORD="highsecurecloudstorage2015"
+EMAIL_HOST_USER="******"
+EMAIL_HOST_PASSWORD="****"
 EMAIL_PORT=587
 # Application definition
 
